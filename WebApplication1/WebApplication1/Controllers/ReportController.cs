9125ce60b1131e455c15adef74bb5a0d8f4abab7
MonaemKhan <khanmonaem07@gmail.com>
2023-11-16 23:13:29 +0600

    RDLC Report Generating Practice

---
+++ WebApplication1/WebApplication1/Controllers/ReportController.cs
@@ -0,0 +1,65 @@
+﻿using AspNetCore.Reporting;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+using System.Text;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReportController : ControllerBase
+{
+    private readonly IWebHostEnvironment _environment;
+
+    public ReportController(IWebHostEnvironment environment)
+    {
+        _environment = environment;
+    }
+
+    [HttpGet]
+    public ActionResult Print()
+    {
+        List<Studemt> dataSource = new List<Studemt>();
+        dataSource.Add(new Studemt
+        {
+            StudentRegNo = 1,
+            StudentName = "monae",
+            StudentEmail = "monae@gmail.com",
+            StudentPhone = "01303",
+            CreatedAt = DateTime.Now,
+            CreatedBy = "m",
+            UpdatedAt = DateTime.Now,
+            UpdatedBy = "m"
+        });
+        
+        dataSource.Add(new Studemt
+        {
+            StudentRegNo = 2,
+            StudentName = "monae1",
+            StudentEmail = "monae1@gmail.com",
+            StudentPhone = "013031",
+            CreatedAt = DateTime.Now,
+            CreatedBy = "m1",
+            UpdatedAt = DateTime.Now,
+            UpdatedBy = "m1"
+        });
+
+        string reportName = "TestReport.pdf"; // report name
+        string reportPath = Path.Combine(_environment.ContentRootPath, "Report", "Report1.rdlc"); // get rdlc path
+
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        Encoding.GetEncoding("utf-8");
+
+        LocalReport report = new LocalReport(reportPath);
+
+        report.AddDataSource("DataSet1", dataSource); // ("DatasetName","DatasetValue")
+
+
+        Dictionary<string, string> parameters = new Dictionary<string, string>();
+        parameters.Add("Name", "Monaem"); // add parameter ("ParameterName","ParameterValue")
+        var result = report.Execute(RenderType.Pdf, 2, parameters); // rendertype, no of parameter including dataset
+
+        return File(result.MainStream, MediaTypeNames.Application.Octet, reportName); // process the file as pdf
+    }
+}
---
+++ WebApplication1/WebApplication1/Model/Studemt.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Model;
+
+public class Studemt
+{
+    public int StudentRegNo { get; set; }
+    public string? StudentName { get; set; }
+    public string? StudentEmail { get; set; }
+    public string? StudentPhone { get; set; }
+    public DateTimeOffset? CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTimeOffset? UpdatedAt { get; set; }
+    public string? UpdatedBy { get; set; }
+}
